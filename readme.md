c204901793176492d869c72fad687c687f3e40a4
Wei Chen <cj1640867082@outlook.com>
2022-06-02 20:43:20 +0800

    优化文件结构

--- readme.md
+++ readme.md
@@ -1,2 +1,33 @@
-姿态评分APP后台服务器
-![23](https://gitee.com/dick121380/my-bed/raw/master/971124BD-3B4F-4DCE-A3CF-986346040C79.jpeg)
+# 姿态评分APP后台服务器
+**服务器调用功能还没弄**
+## 文件结构
+```shell
+.
+├── main.py
+├── readme.md
+├── requirements.txt
+├── StdPoseDatabase
+│   ├── Images
+│   ├── Management.txt
+│   └── Points
+├── test_main.py
+└── utils
+    ├── CalculateAngle.py
+    ├── DataBase.py
+    └── Score.py
+```
+* 主程序运行在`main.py`
+* utils存放函数
+    * `CalculateAngle.py`计算关节角度。
+    * `DataBase.py`用于管理数据。主要是当数据库中增加或者删除标准数据时，自动生成或删除姿势点数据。**当前未实现**。
+    * `score.py`为动作打分。只实现了最朴素的打分方法。
+* StdPoseDatabase存放数据文件
+    * Images存放标准姿势图片
+    * Points存放标准姿势点数据
+
+详细函数实现见代码注释(如果有写注释的话)
+
+---
+## 角度定义
+![角度定义](https://markdown-bed-pixel.oss-cn-shanghai.aliyuncs.com/public/971124BD-3B4F-4DCE-A3CF-986346040C79.jpeg)
+---
--- test_main.py
+++ test_main.py
@@ -1,24 +1,44 @@
+import sys
+from utils.DataBase import check_database
+from utils.CalculateAngle import get_elbow_L,get_elbow_R,get_hip_L,get_hip_R,get_knee_L,get_knee_R,get_shoulder_L,get_shoulder_R
+from utils.Score import get_score
 import mediapipe as mp
 import numpy as np
 import cv2 as cv
-from utils.CalculateAngle import get_shoulder_L
-mp_pose=mp.solutions.pose
-mp_drawing=mp.solutions.drawing_utils
-pose=mp_pose.Pose(static_image_mode=False,enable_segmentation=True,min_detection_confidence=0.5)
-capture=cv.VideoCapture(0)
-cv.namedWindow("img",0)
-while capture.isOpened():
-    ret,frame=capture.read()
-    if not ret:
-        break
-    rgb_image=cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    results=pose.process(rgb_image)
-    print("shoulder_L:",get_shoulder_L(results.pose_landmarks.landmark))
+
+
+std=np.array([90,90,90,90])  # 测试动作为举哑铃动作，要求肩关节和肘关节90度
+w=np.array([0.25,0.25,0.25,0.25])
+if __name__=="__main__":
+    mp_pose=mp.solutions.pose
+    mp_drawing=mp.solutions.drawing_utils
+    pose_detector=mp_pose.Pose(static_image_mode=False,enable_segmentation=True,min_detection_confidence=0.5)
     
-    mp_drawing.draw_landmarks(frame, results.pose_landmarks)
-    cv.imshow("img", frame)
-    key=cv.waitKey(1)
-    if key==27:
-        cv.destroyAllWindows()
-        capture.release()
-        break
+    video_capture=cv.VideoCapture(0)
+    cv.namedWindow("img",0)
+    while video_capture.isOpened():
+        ret,frame=video_capture.read()
+        if not ret:
+            break
+        rgb_frame=cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        detect_result=pose_detector.process(rgb_frame)
+        pose_landmark=detect_result.pose_landmarks.landmark
+        shoulder_L=get_shoulder_L(pose_landmark)
+        shoulder_R=get_shoulder_R(pose_landmark)
+        elbow_L=get_elbow_L(pose_landmark)
+        elbow_R=get_elbow_R(pose_landmark)
+        score=get_score(standard_angle=std,measured_angle=np.array([shoulder_L,shoulder_R,elbow_L,elbow_R]),weights=w)
+
+        mp_drawing.draw_landmarks(image=frame, 
+            landmark_list=detect_result.pose_landmarks,
+            connections=mp_pose.POSE_CONNECTIONS
+        )
+        frame=cv.flip(frame, 1)  # 水平翻转画面
+        cv.imshow("img", frame)
+        print("Your Score is:%.2f\n"%(score*100))
+        key=cv.waitKey(1)
+        if key==27:
+            cv.destroyAllWindows()
+            video_capture.release()
+            break
